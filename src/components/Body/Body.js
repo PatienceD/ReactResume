83347fa25e3c2103b04a48712fe51969b730a373
Patience Deaton <46998183+PatienceD@users.noreply.github.com>
2019-11-25 15:31:16 -0500

    Adding features
    Added content to footer, also added header image and name

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,12 +1,21 @@
 import React from "react";
 import "./Body.css";
+import logo from "../Images/laptop.jpg";
 
 function Body() {
   return (
     <div>
       <div className="row">
-          
+        <div className='frame'>
+          <img src={logo} className="head-image" alt="Responsive image"></img>
+          <div className="intro">
+            <p>Patience Deaton</p>
+            <hr></hr>
+            <h1>Full Stack Developer</h1>
+          </div>
+        </div>
       </div>
+      
     </div>
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,40 @@
 import React from "react";
 import "./Footer.css";
 
-function Footer(){
-    return(
+function Footer() {
+    return (
         <div className="footer">
-            <h1>This is a test</h1>
+            <div className="row">
+                <div className="col-md-4">
+                    <h3>Social Media</h3>
+                    <hr></hr>
+                    <ul id="social-media">
+                        <li>Linkedin <i className="fab fa-linkedin"></i></li>
+                        <li>GitHub <i className="fab fa-github-square"></i></li>
+                        <li>FaceBook <i className="fab fa-facebook-square"></i></li>
+                    </ul>
+                </div>
+                <div className="col-md-4">
+                    <h3>Get in Touch with Me</h3>
+                    <hr></hr>
+                    <ul id="contact-info">
+                        <li>Orlando, FL</li>
+                        <li>Email: patience.deaton2@outlook.com</li>
+                        <li>(603)921-1506</li>
+                    </ul>
+                </div>
+                <div className="col-md-4">
+                    <h3>Navigation</h3>
+                    <hr></hr>
+                    <ul id="footer-nav">
+                        <li>About Me</li>
+                        <li>Skills</li>
+                        <li>Projects</li>
+                        <li>Testimonials</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </div>
         </div>
 
     );
